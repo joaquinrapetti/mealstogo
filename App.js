5733af7f4ebf4b2f2ef3ca016ee0ee434f5eaae1
joaquinrapetti <j.rapetti92@gmail.com>
2022-08-10 16:30:24 -0300

    app layout - ios and android statusbar adjustments

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/* ExpoStatusBar only ios adjustment */}
+      <ExpoStatusBar />
+      <SafeAreaView style={styles.rootContainer}>
+        <View style={styles.searchContainer}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.listContainer}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
+console.log(StatusBar.currentHeight);
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // marginTop: Platform.OS === "android" ? 50 : null, -- Manual distance from status bar
+    // StatusBar.currentHeight -- only android currentHeight from status bar
+    marginTop: StatusBar.currentHeight,
+  },
+  searchContainer: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  listContainer: {
+    flex: 1, // flex: 1 - Extends all as auto is dynamic ; '100%' is overflow
+    padding: 16,
+    backgroundColor: "red",
+    height: "auto",
   },
 });
